1dfd82c6378e0ce1d4efd870858623f5fe7fa2a9
Hannah Yockel <hannahjyockel@gmail.com>
2022-09-07 15:51:39 -0500

    project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import { Route, Routes } from "react-router-dom"
+import { Container } from "react-bootstrap"
+
 function App() {
-  return <h1>Hi</h1>
+  return <Container>Hi</Container>
 }
 
 export default App
