9aa019e04458aea906f2dd8fc4b613800d9d25bf
hannahyockel <88843058+hannahyockel@users.noreply.github.com>
2022-09-07 19:16:36 -0500

    add routes and navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import { Container } from "react-bootstrap"
+
+export function Navbar() {
+    return (
+        <Container>
+            Navbar
+        </Container>
+    )
+}
---
+++ src/pages/Store.tsx
@@ -0,0 +1,9 @@
+import { Container } from "react-bootstrap"
+
+export function Store() {
+    return (
+        <Container>
+            <h1>Store</h1>
+        </Container>
+    )
+}
